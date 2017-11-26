233c561d91361814f6bca9827dd2f2a7f4f99494
inguy007 <inguy007@gmail.com>
2017-11-26 17:42:19 +0530

    Add files via upload
    initial commit

---
+++ src/main/java/com/test/spring/boot/application/EurekaServerApplication.java
@@ -0,0 +1,46 @@
+package com.test.spring.boot.application;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(EurekaServerApplication.class);
+
+	@Autowired
+	private Environment env;
+
+	public static void main(String[] args) throws UnknownHostException {
+		LOGGER.info("Starting spring eureka server");
+		SpringApplication app = new SpringApplication(EurekaServerApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		String protocol = "http";
+		if (env.getProperty("server.ssl.key-store") != null) {
+			protocol = "https";
+		}
+		LOGGER.info(
+				"\n----------------------------------------------------------\n\t"
+						+ "Application '{}' is running! Access URLs:\n\t" + "Local: \t\t{}://localhost:{}\n\t"
+						+ "External: \t{}://{}:{}\n\t"
+						+ "Profile(s): \t{}\n----------------------------------------------------------",
+				env.getProperty("spring.application.name"), protocol, env.getProperty("server.port"), protocol,
+				InetAddress.getLocalHost().getHostAddress(), env.getProperty("server.port"), env.getActiveProfiles());
+
+		String configServerStatus = env.getProperty("configserver.status");
+		LOGGER.info(
+				"\n----------------------------------------------------------\n\t"
+						+ "Config Server: \t{}\n----------------------------------------------------------",
+				configServerStatus == null ? "Not found or not setup for this application" : configServerStatus);
+	}
+
+}
